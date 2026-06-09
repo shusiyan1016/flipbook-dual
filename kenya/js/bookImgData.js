4679019dad8ea341e470a4f97157b4c2382e2061
shusiyan1016 <shusiyan1016@users.noreply.github.com>
2026-06-09 15:11:01 +0800

    restore_iframe

---
+++ kenya/js/bookImgData.js
@@ -0,0 +1,27 @@
+// 书本目录
+var ols = [
+  {"caption": "封面", "page": "1"},
+  {"caption": "灰页", "page": "2"},
+  {"caption": "扉页", "page": "3"},
+  {"caption": "第 1 节", "page": "4"},
+  {"caption": "第 2 节", "page": "5"},
+  {"caption": "第 3 节", "page": "6"},
+  {"caption": "第 4 节", "page": "7"},
+  {"caption": "第 5 节", "page": "8"},
+  {"caption": "第 6 节", "page": "9"},
+  {"caption": "第 7 节", "page": "10"},
+  {"caption": "第 8 节", "page": "11"},
+  {"caption": "第 9 节", "page": "12"},
+  {"caption": "第 10 节", "page": "13"},
+  {"caption": "第 11 节", "page": "14"},
+  {"caption": "封底", "page": "15"},
+  {"caption": "灰页", "page": "16"},
+];
+
+// 路径配置
+var loadImgpath = "./files/thumb/"
+
+bookConfig.largePath = loadImgpath
+bookConfig.normalPath = loadImgpath
+bookConfig.thumbPath = loadImgpath
+bookConfig.totalPageCount = ols.length
