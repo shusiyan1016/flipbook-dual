ee48cb86efa7aa9858a5df6e4d0d46ab60ad6c24
shusiyan1016 <shusiyan1016@users.noreply.github.com>
2026-06-09 22:32:02 +0800

    fix TOC page mapping and visual style

--- donald/js/bookImgData.js
+++ donald/js/bookImgData.js
@@ -2,13 +2,13 @@
 var ols = [
   {"caption": "封面", "page": "1"},
   {"caption": "目录", "page": "2"},
-  {"caption": "01 进入显性设计 —— P1", "page": "3"},
-  {"caption": "02 唐纳德·诺曼是谁 —— P2", "page": "4"},
-  {"caption": "03 情感化设计三层次 —— P3", "page": "5"},
-  {"caption": "04 与诺曼对话 —— P5", "page": "6"},
-  {"caption": "05 你的设计练习 —— P6", "page": "7"},
-  {"caption": "06 显性设计自评 —— P8", "page": "8"},
-  {"caption": "07 转向隐性设计 —— P9", "page": "9"},
+  {"caption": "01 进入显性设计 —— P1", "page": "4"},
+  {"caption": "02 唐纳德·诺曼是谁 —— P2", "page": "5"},
+  {"caption": "03 情感化设计三层次 —— P3", "page": "6"},
+  {"caption": "04 与诺曼对话 —— P5", "page": "8"},
+  {"caption": "05 你的设计练习 —— P6", "page": "9"},
+  {"caption": "06 显性设计自评 —— P8", "page": "11"},
+  {"caption": "07 转向隐性设计 —— P9", "page": "12"},
 ];
 
 // 路径配置
